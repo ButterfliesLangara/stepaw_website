6fca2fbdded2f6babc81cfc7d569231fa4c6e760
Maneesh <thouti.maneesh43@gmail.com>
2021-11-10 17:35:28 -0800

    feat:scss,normalize.scss added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Semantic_components/Header";
+import Body from "./Components/Semantic_components/Body";
+import Footer from "./Components/Semantic_components/Footer";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
