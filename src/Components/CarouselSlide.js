b4d9db1c4826eab4551aa70398e2a2351d114030
Maneesh <thouti.maneesh43@gmail.com>
2021-11-25 01:00:29 -0800

    Changes to slider

--- src/Components/CarouselSlide.js
+++ src/Components/CarouselSlide.js
@@ -1,18 +1,39 @@
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
+
 import carousel_image_1 from '../assets/carousel_image_1.svg';
 import carousel_image_2 from '../assets/carousel_image_2.svg'
 import carousel_image_3 from '../assets/carousel_image_3.svg'
-
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from "./Image";
+import SwiperCore, {
+  } from 'swiper';
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 const CarouselSlide=()=>{
+    
     return(
 <div className="slide-show">
-    <AliceCarousel autoPlay autoPlayInterval="3000" activeIndex="0">
-        <Image src={carousel_image_1} alt={'Carousel image'}/>
-        <Image src={carousel_image_2} alt={'Carousel image'}/>
-        <Image src={carousel_image_3} alt={'Carousel image'}/>
-    </AliceCarousel>
+<Swiper
+modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      pagination={{ clickable: true }}
+        slidesPerView={1}
+        navigation
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+<Image src={carousel_image_1}/>
+
+      </SwiperSlide>
+      <SwiperSlide>
+          <Image src={carousel_image_2}/>
+      </SwiperSlide>
+      <SwiperSlide>
+          <Image src={carousel_image_3}/>
+      </SwiperSlide>
+      ...
+    </Swiper>
 </div>
     )
 }
