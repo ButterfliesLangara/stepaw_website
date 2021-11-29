af59164b30468076b02bb47151d1ecca69708ee3
Maneesh <thouti.maneesh43@gmail.com>
2021-11-28 23:52:38 -0800

    fix:smooth scroll polyfill for ios/macos added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Semantic_components/Header";
 import Body from "./Components/Semantic_components/Body";
 import Footer from "./Components/Semantic_components/Footer";
+import smoothscroll from 'smoothscroll-polyfill';
 import "./App.scss";
 
 function App() {
+smoothscroll.polyfill();
   return (
     <div className="App">
       <Header />
